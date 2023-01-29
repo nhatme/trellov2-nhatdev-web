354986887893c1211e686e4e685fb7d9135074e2
nhatme <nhatchimp12@gmail.com>
2023-01-30 01:58:48 +0700

    clean code

---
+++ src/components/BoardBar/BoardBar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './BoardBar.scss';
+
+const BoardBar = () => {
+    return (
+        <nav className='navbar-board'>BoardBar</nav>
+    )
+}
+
+export default BoardBar
---
+++ src/components/Task/Task.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Task.scss'
+
+const Task = () => {
+    return (
+        <li className='task-item'>
+            <img src='https://i.guim.co.uk/img/media/63de40b99577af9b867a9c57555a432632ba760b/0_266_5616_3370/master/5616.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=93458bbe24b9f88451ea08197888ab8e' alt='nhatdevweb' />
+            Title: NHATDEV
+        </li>
+    )
+}
+
+export default Task
