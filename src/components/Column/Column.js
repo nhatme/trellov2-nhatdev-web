9cebf32d63d24189adc60b29d90a9ef0c78ef374
nhatme <nhatchimp12@gmail.com>
2023-02-02 21:59:42 +0700

    Handle state

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -7,12 +7,8 @@ import { mapOrder } from 'utilities/sorts'
 
 const Column = (props) => {
 
-    const { column } = props
+    const { column, onCardDrop } = props
     const cards = mapOrder(column.cards, column.cardOrder, "id")
-
-    const onCardDrop = (dropResult) => {
-        console.log(dropResult)
-    }
 
     return (
         <div className='column'>
@@ -21,7 +17,7 @@ const Column = (props) => {
                 <Container
                     orientation="vertical"
                     groupName="nhatdev-columns"
-                    onDrop={onCardDrop}
+                    onDrop={dropResult => onCardDrop(column.id, dropResult)}
                     getChildPayload={index => cards[index]}
                     dragClass="card-ghost"
                     dropClass="card-ghost-drop"
@@ -40,7 +36,11 @@ const Column = (props) => {
 
                 </ Container >
             </div>
-            <footer>Add another card</footer>
+            <footer>
+                <div className='footer-actions'>
+                    < i className='fa fa-plus icon' /> Add another card
+                </div>
+            </footer>
         </div>
     )
 }
